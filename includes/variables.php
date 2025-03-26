5c70ac3b86ac94517f63db9456876626cde424d3
DennisUden <162509535+DennisUden@users.noreply.github.com>
2025-03-26 10:20:24 +0100

    Update variables.php

--- includes/variables.php
+++ includes/variables.php
@@ -1,27 +1,49 @@
 <?php
 define('NAV_PATH', $_SERVER['DOCUMENT_ROOT'].'/includes/nav.php');
-define('FOOTER_PATH', $_SERVER['DOCUMENT_ROOT'].'/includes/footer.php');	
-define('HEAD_PATH', $_SERVER['DOCUMENT_ROOT'].'/includes/head.php');	
+define('FOOTER_PATH', $_SERVER['DOCUMENT_ROOT'].'/includes/footer.php');
+define('HEAD_PATH', $_SERVER['DOCUMENT_ROOT'].'/includes/head.php');
 
-switch ($path) {
-	case '/kontakt':
-		$title		= 'UDENIT - Kontakt';
-		$description	= 'Kontaktieren Sie UDENIT für IT-Beratung und Dienstleistungen.';
-		break;
-	case '/loesungen':
-		$title		= 'UDENIT - Lösungen';
-		$description	= 'Unsere IT-Lösungen für Ihr Unternehmen.';
-		break;
-	case '/impressum':
-		$title		= 'UDENIT - Impressum';
-		$description	= 'Impressum von UDENIT.';
-		break;
-	case '/datenschutz':
-		$title		= 'UDENIT - Datenschutz';
-		$description	= 'Datenschutzerklärung von UDENIT.';
-		break;
-	default:
-		$title		= 'UDENIT';
-		$description 	= 'Startseite von UDENIT. IT-Beratung und Dienstleistungen in der Gemeinde Ihlow.';
-}
+$routes = [
+    '/' => [
+        'controller' => 'index.php',
+        'title' => 'UDENIT',
+        'description' => 'Startseite von UDENIT. IT-Beratung und Dienstleistungen in der Gemeinde Ihlow.'
+    ],
+    '/index' => [
+        'controller' => 'index.php',
+        'title' => 'UDENIT',
+        'description' => 'Startseite von UDENIT. IT-Beratung und Dienstleistungen in der Gemeinde Ihlow.'
+    ],
+    '/index.php' => [
+        'controller' => 'index.php',
+        'title' => 'UDENIT',
+        'description' => 'Startseite von UDENIT. IT-Beratung und Dienstleistungen in der Gemeinde Ihlow.'
+    ],
+    '/loesungen' => [
+        'controller' => 'loesungen.php',
+        'title' => 'UDENIT - Lösungen',
+        'description' => 'Unsere IT-Lösungen für Ihr Unternehmen.'
+    ],
+    '/kontakt' => [
+        'controller' => 'kontakt.php',
+        'title' => 'UDENIT - Kontakt',
+        'description' => 'Kontaktieren Sie UDENIT für IT-Beratung und Dienstleistungen.'
+    ],
+    '/impressum' => [
+        'controller' => 'impressum.php',
+        'title' => 'UDENIT - Impressum',
+        'description' => 'Impressum von UDENIT.'
+    ],
+    '/datenschutz' => [
+        'controller' => 'datenschutz.php',
+        'title' => 'UDENIT - Datenschutz',
+        'description' => 'Datenschutzerklärung von UDENIT.'
+    ]
+];
+
+$route = $routes[$path] ?? $routes['/'];
+$title = $route['title'];
+$description = $route['description'];
+$controller = $_SERVER['DOCUMENT_ROOT'] . '/controllers/' . $route['controller'];
+
 ?>
