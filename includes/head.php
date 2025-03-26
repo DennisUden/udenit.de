1ad971c62438b69534a24ae99b02b11d0b490d43
Dennis Uden <162509535+DennisUden@users.noreply.github.com>
2025-03-26 11:20:37 +0100

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php echo $title; ?></title>
         <link rel="stylesheet" href="/styles/global.css" type="text/css">
-	<link rel="canonical" href="<?php echo 'https://udenit.de'.$path; ?>">
+	<link rel="canonical" href="<?php echo 'https://udenit.de'.$route['canonical']; ?>">
 	<meta name="description" content="<?php echo $description; ?>">
 	<link rel="icon" type="image/png" href="/favicon.png" sizes="16x16">
     </head>
