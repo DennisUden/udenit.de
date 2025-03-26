ae7fe9e43adfd66ca355f19b4d3e417a02cb1f77
DennisUden <162509535+DennisUden@users.noreply.github.com>
2025-03-26 10:18:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,20 +4,5 @@ $path	= parse_url($uri, PHP_URL_PATH);
 
 require_once $_SERVER['DOCUMENT_ROOT'].'/includes/variables.php';
 
-switch ($path) {
-	case '/loesungen':
-		require $_SERVER['DOCUMENT_ROOT'].'/controllers/loesungen.php';
-		break;
-	case '/kontakt':
-		require $_SERVER['DOCUMENT_ROOT'].'/controllers/kontakt.php';
-		break;
-	case '/impressum':
-		require $_SERVER['DOCUMENT_ROOT'].'/controllers/impressum.php';
-		break;
-	case '/datenschutz':
-		require $_SERVER['DOCUMENT_ROOT'].'/controllers/datenschutz.php';
-		break;
-	default:
-		require $_SERVER['DOCUMENT_ROOT'].'/controllers/index.php';
-}
+require $controller
 ?>
