e1077c2583e9f140799cbf5da38efa9476d79cd7
Dennis Uden <162509535+DennisUden@users.noreply.github.com>
2025-03-26 23:51:45 +0100

    Update nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,7 @@
 <header>
     <nav>
         <ul>
-            <li><a href="/index">Startseite</a></li>
+            <li><a href="/">Startseite</a></li>
             <li><a href="/loesungen">Lösungen</a></li>
             <li><a href="/kontakt">Kontakt</a></li>
         </ul>
