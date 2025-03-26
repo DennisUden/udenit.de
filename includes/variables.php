7d85ea8eab2e71eff53f2680ecc552c614b0f414
DennisUden <162509535+DennisUden@users.noreply.github.com>
2025-03-26 10:59:32 +0100

    Update variables.php

--- includes/variables.php
+++ includes/variables.php
@@ -1,34 +1,34 @@
 <?php
-define('NAV_PATH', $_SERVER['DOCUMENT_ROOT'].'/includes/nav.php');
-define('FOOTER_PATH', $_SERVER['DOCUMENT_ROOT'].'/includes/footer.php');
-define('HEAD_PATH', $_SERVER['DOCUMENT_ROOT'].'/includes/head.php');
+define('NAV_PATH',      $_SERVER['DOCUMENT_ROOT'].'/includes/nav.php');
+define('FOOTER_PATH',   $_SERVER['DOCUMENT_ROOT'].'/includes/footer.php');
+define('HEAD_PATH',     $_SERVER['DOCUMENT_ROOT'].'/includes/head.php');
 
 $defaultRoute = [
-        'controller' => 'index.php',
-        'title' => 'UDENIT',
-        'description' => 'Startseite von UDENIT. IT-Beratung und Dienstleistungen in der Gemeinde Ihlow.'
+        'controller'    => 'index.php',
+        'title'         => 'UDENIT',
+        'description'   => 'Startseite von UDENIT. IT-Beratung und Dienstleistungen in der Gemeinde Ihlow.'
 ];
 
 $routes = [
     '/loesungen' => [
-        'controller' => 'loesungen.php',
-        'title' => 'UDENIT - Lösungen',
-        'description' => 'Unsere IT-Lösungen für Ihr Unternehmen.'
+        'controller'    => 'loesungen.php',
+        'title'         => 'UDENIT - Lösungen',
+        'description'   => 'Unsere IT-Lösungen für Ihr Unternehmen.'
     ],
     '/kontakt' => [
-        'controller' => 'kontakt.php',
-        'title' => 'UDENIT - Kontakt',
-        'description' => 'Kontaktieren Sie UDENIT für IT-Beratung und Dienstleistungen.'
+        'controller'    => 'kontakt.php',
+        'title'         => 'UDENIT - Kontakt',
+        'description'   => 'Kontaktieren Sie UDENIT für IT-Beratung und Dienstleistungen.'
     ],
     '/impressum' => [
-        'controller' => 'impressum.php',
-        'title' => 'UDENIT - Impressum',
-        'description' => 'Impressum von UDENIT.'
+        'controller'    => 'impressum.php',
+        'title'         => 'UDENIT - Impressum',
+        'description'   => 'Impressum von UDENIT.'
     ],
     '/datenschutz' => [
-        'controller' => 'datenschutz.php',
-        'title' => 'UDENIT - Datenschutz',
-        'description' => 'Datenschutzerklärung von UDENIT.'
+        'controller'    => 'datenschutz.php',
+        'title'         => 'UDENIT - Datenschutz',
+        'description'   => 'Datenschutzerklärung von UDENIT.'
     ]
 ];
 
