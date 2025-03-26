8384e59db43bbc93b8da8f0a1f9a25559965db5b
DennisUden <162509535+DennisUden@users.noreply.github.com>
2025-03-26 09:24:54 +0100

    Update variables.php

--- includes/variables.php
+++ includes/variables.php
@@ -4,12 +4,6 @@ define('FOOTER_PATH', $_SERVER['DOCUMENT_ROOT'].'/includes/footer.php');
 define('HEAD_PATH', $_SERVER['DOCUMENT_ROOT'].'/includes/head.php');	
 
 switch ($path) {
-	case '/':
-	case '/index':
-	case '/index.php':
-		$title		= 'UDENIT';
-		$description 	= 'Startseite von UDENIT. IT-Beratung und Dienstleistungen in der Gemeinde Ihlow.';
-		break;
 	case '/kontakt':
 		$title		= 'UDENIT - Kontakt';
 		$description	= 'Kontaktieren Sie UDENIT für IT-Beratung und Dienstleistungen.';
