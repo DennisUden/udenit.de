5b14c1c12d714d09f9b65549a9f030f8b604ae1b
Dennis Uden <162509535+DennisUden@users.noreply.github.com>
2025-03-27 21:49:35 +0100

    Update meta description for seo in variables.php

--- includes/variables.php
+++ includes/variables.php
@@ -6,7 +6,7 @@ define('HEAD_PATH',     $_SERVER['DOCUMENT_ROOT'].'/includes/head.php');
 $defaultRoute = [
         'controller'    => 'index.php',
         'title'         => 'UDENIT - IT-Beratung und Dienstleistungen in Ihlow',
-        'description'   => 'Startseite von UDENIT. IT-Beratung und Dienstleistungen in der Gemeinde Ihlow.',
+        'description'   => 'UDENIT bietet IT-Beratung und Servermanagement in Ihlow, Ostfriesland – Ihr Experte für Netzwerke und IT-Lösungen.',
         'canonical'     => '/'
 ];
 
@@ -14,25 +14,25 @@ $routes = [
     '/loesungen' => [
         'controller'    => 'loesungen.php',
         'title'         => 'UDENIT - IT-Lösungen für Unternehmen in Ihlow',
-        'description'   => 'Unsere IT-Lösungen für Ihr Unternehmen.',
+        'description'   => 'IT-Lösungen für Firmen in Ihlow: Servermanagement, Netzwerkadministration und IT-Beratung von UDENIT in Ostfriesland.',
         'canonical'     => '/loesungen'
     ],
     '/kontakt' => [
         'controller'    => 'kontakt.php',
         'title'         => 'UDENIT - Kontakt für IT-Beratung in Ihlow',
-        'description'   => 'Kontaktieren Sie UDENIT für IT-Beratung und Dienstleistungen.',
+        'description'   => 'Kontaktieren Sie UDENIT für IT-Beratung, Servermanagement und Netzwerklösungen in Ihlow, Ostfriesland – Ihr IT-Experte!',
         'canonical'     => '/kontakt'
     ],
     '/impressum' => [
         'controller'    => 'impressum.php',
         'title'         => 'UDENIT - Impressum',
-        'description'   => 'Impressum von UDENIT.',
+        'description'   => 'Impressum von UDENIT – Ihr Anbieter für IT-Beratung, Servermanagement und Netzwerke in Ihlow, Ostfriesland. Rechtliche Infos.',
         'canonical'     => '/impressum'
     ],
     '/datenschutz' => [
         'controller'    => 'datenschutz.php',
         'title'         => 'UDENIT - Datenschutzerklärung',
-        'description'   => 'Datenschutzerklärung von UDENIT.',
+        'description'   => 'Datenschutz bei UDENIT: Servermanagement und IT-Beratung in Ihlow, Ostfriesland – Ihre Daten sicher in unseren Händen.',
         'canonical'     => '/datenschutz'
     ]
 ];
