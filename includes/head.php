af7a0a48c927bf45379ce7ae8560b40ea19a2c89
Dennis Uden <162509535+DennisUden@users.noreply.github.com>
2025-04-01 13:59:38 +0200

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="de">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php echo $title; ?></title>
-	<meta property="og:site_name" content="UdenIT">
-        <link rel="stylesheet" href="/styles/global.css" type="text/css">
-	<link rel="canonical" href="<?php echo 'https://udenit.de'.$route['canonical']; ?>">
-	<meta name="description" content="<?php echo $description; ?>">
-	<link rel="icon" type="image/png" href="/favicon.png" sizes="16x16">
+        <meta charset=  "UTF-8">
+        <meta name=     "viewport"      content="width=device-width, initial-scale=1.0">
+        <title><?php echo $title; ?></title>
+        <meta property= "og:site_name"  content="UdenIT">
+        <meta property= "og:type"       content="website">
+        <link rel=      "stylesheet"    href="/styles/global.css"       type="text/css">
+        <link rel=      "canonical"     href="<?php echo 'https://udenit.de'.$route['canonical']; ?>">
+        <meta property= "og:url"        content="<?php echo 'https://udenit.de'.$route['canonical']; ?>">
+        <meta name=     "description"   content="<?php echo $description; ?>">
+        <meta property= "og:description"content="<?php echo $description; ?>">
+        <link rel=      "icon"          type="image/png"        href="/favicon.png"     sizes="16x16">
     </head>
     <body>
