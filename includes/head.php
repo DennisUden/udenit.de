8adedec9d0adffc8b593583fc3a8f79fcb77db07
Dennis Uden <162509535+DennisUden@users.noreply.github.com>
2025-04-01 11:07:48 +0200

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php echo $title; ?></title>
+	<meta property="og:site_name" content="UdenIT">
         <link rel="stylesheet" href="/styles/global.css" type="text/css">
 	<link rel="canonical" href="<?php echo 'https://udenit.de'.$route['canonical']; ?>">
 	<meta name="description" content="<?php echo $description; ?>">
