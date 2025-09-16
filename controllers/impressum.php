0edd4f47032ff8bbc1ebda30c1507f5d70108492
Dennis Uden <162509535+DennisUden@users.noreply.github.com>
2025-09-16 13:37:51 +0200

    Update impressum.php

--- controllers/impressum.php
+++ controllers/impressum.php
@@ -8,9 +8,9 @@
 			<h2>UDENIT<br></h2>
 			<p>Dennis Uden<br>Norderwieke 41<br>26632 Ihlow<br>Deutschland<br></p>
 			<p>Telefon: +49 15154735320<br>E-Mail: info@udenit.de</p>
-            		<p>USt-Id: DE313508145</p>
 		</div>
 	</div>
 </div>
 
 <?php include FOOTER_PATH; ?>
+
