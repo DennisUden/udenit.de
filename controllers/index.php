08286e3436861e1a548333f04c83701fe7b3f859
Dennis Uden <162509535+DennisUden@users.noreply.github.com>
2025-09-17 11:00:12 +0200

    Update index.php

--- controllers/index.php
+++ controllers/index.php
@@ -10,8 +10,11 @@
 
 		<div class="blocktext">
 			<h2>Ihr Partner für Fragen rund um das Thema Unternehmens-IT</h2>
-			<p>Spezialisiert auf die Bedürfnisse von KMU (Kleine und mittlere Unternehmen), bietet UdenIT effiziente Lösungen in den Bereichen Email, Automatisierung, Compliance, Hosting, ERP, E-Commerce und weiteren Gebieten.</p>
+			<p>Spezialisiert auf die Bedürfnisse von KMU (Kleine und mittlere Unternehmen), bietet UdenIT effiziente Lösungen in den Bereichen Email, Automatisierung, Compliance, Hosting, ERP (speziell aber nicht auschließlich das JTL-Ökosystem), E-Commerce und weiteren Gebieten.</p>
 			<br>
+
+			<h2>E-Rechnung Implementierung</h2>
+			<p>Die Pflicht zur E-Rechnung stellt viele Unternehmen vor Herausforderungen. UdenIT unterstützt Sie Ihre Prozesse gesetzeskonform anzupassen. Unter anderem sind JTL-Wawi Export-Vorlagen für 50€ erwerbbar.</p>
 
 			<h2>Hintergrund von Produktfotos entfernen</h2>
 			<p>Mit der automatisierten Lösung von UdenIT kein Problem mehr. Einfach in den Bilder-Ordner navigieren, Programm ausführen und die Bilder sind fertig zur Benutzung. Die Einrichtungskosten pro Arbeitsplatz belaufen sich auf 100€, die weitere Benutzung ist kostenlos. Das Programm läuft unter Windows, Linux und MacOS.</p>
